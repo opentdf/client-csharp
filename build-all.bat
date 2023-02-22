39112d7bd075d13326f48dc06df8b085fd6fc937
Pat Mancuso <pmancuso@virtru.com>
2023-02-21 21:52:33 -0500

    Add version to zipfile name

--- build-all.bat
+++ build-all.bat
@@ -17,20 +17,22 @@ conan build .. --build-folder .
 set builderrorlevel=%errorlevel%
 if %builderrorlevel% neq 0 goto fin
 
-REM create the nuget package?
+REM create the nuget package
 cmake --build . --target PACKAGE
 set builderrorlevel=%errorlevel%
 if %builderrorlevel% neq 0 goto fin
 
 REM for some reason the cmake install isn't working, so copy these to the install area now
-mkdir %PROJECT_DIR%\%TDF_CSHARP_OUTPUT%\include\
-copy /y %PROJECT_DIR%\%TDF_CMAKE_BUILD_DIR%\csharp-bindings\source\*.cs %PROJECT_DIR%\%TDF_CSHARP_OUTPUT%\include\
+mkdir %PROJECT_DIR%%TDF_CSHARP_OUTPUT%\include\
+copy /y %PROJECT_DIR%%TDF_CMAKE_BUILD_DIR%\csharp-bindings\source\*.cs %PROJECT_DIR%%TDF_CSHARP_OUTPUT%\include\
 
-mkdir %PROJECT_DIR%\%TDF_CSHARP_OUTPUT%\lib\
-copy /y %PROJECT_DIR%\%TDF_CMAKE_BUILD_DIR%\lib\*.lib %PROJECT_DIR%\%TDF_CSHARP_OUTPUT%\lib\
-copy /y %PROJECT_DIR%\%TDF_CMAKE_BUILD_DIR%\lib\*.dll %PROJECT_DIR%\%TDF_CSHARP_OUTPUT%\lib\
+mkdir %PROJECT_DIR%%TDF_CSHARP_OUTPUT%\lib\
+copy /y %PROJECT_DIR%%TDF_CMAKE_BUILD_DIR%\lib\*.lib %PROJECT_DIR%%TDF_CSHARP_OUTPUT%\lib\
+copy /y %PROJECT_DIR%%TDF_CMAKE_BUILD_DIR%\lib\*.dll %PROJECT_DIR%%TDF_CSHARP_OUTPUT%\lib\
 
-powershell -command Compress-Archive -Force -Path %PROJECT_DIR%\%TDF_CSHARP_OUTPUT%\ -CompressionLevel Optimal -DestinationPath %PROJECT_DIR%\%TDF_CSHARP_OUTPUT%%TDF_ZIP_SUFFIX%.zip
+:dozip
+set /P TDF_ZIP_SUFFIX=< %PROJECT_DIR%VERSION
+powershell -command Compress-Archive -Force -Path %PROJECT_DIR%%TDF_CSHARP_OUTPUT%\ -CompressionLevel Optimal -DestinationPath %PROJECT_DIR%%TDF_CSHARP_OUTPUT%-%TDF_ZIP_SUFFIX%.zip
 
 :fin
 REM return to where we came from
